a7d140baf6ce9f70a09429e23a8a3e86f2843b8a
ambkw <224786174+ambkw@users.noreply.github.com>
2026-03-26 12:15:17 +0000

    [widget] add public keys

--- src/config/widget.ts
+++ src/config/widget.ts
@@ -1,5 +1,5 @@
 // Clés publiques du widget Bodylink (Xplore Deciplus)
 // Ces clés sont volontairement exposées côté frontend.
-export const BODYLINK_PUBLIC_KEY = "******";
-export const BODYLINK_WIDGET_KEY = "****";
+export const BODYLINK_PUBLIC_KEY = "pk_chain_4gn7aqJgTfdb4jDUcxZrjy5OvB78CS";
+export const BODYLINK_WIDGET_KEY = "wk_eIh43sVMzvRodR2i";
 export const BODYLINK_SCRIPT_URL = "https://cdn.bodylink.fr/widget.js";
